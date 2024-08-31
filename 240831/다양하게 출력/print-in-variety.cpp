0c10b6e5a64ec1e21ac761fd4ede1feb7ea2f04a
good-thing <127587311+good-thing@users.noreply.github.com>
2024-08-31 14:47:52 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240831/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+
+    int a = 365;
+    double b = 3.1415926535;
+
+    cout.precision(11);
+
+    cout << "Total days in Year\n" << a << "\n" << "Circumference rate\n" << b << endl;
+
+    return 0;
+}
